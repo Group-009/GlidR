c07d5b8462cebd8091e19aa2067c849aece53a72
Keir <keir.r.lewis@gmail.com>
2018-05-17 16:16:26 +0100

    HomePage Update

--- uk/ac/cam/mcksj/front/HomePage.java
+++ uk/ac/cam/mcksj/front/HomePage.java
@@ -12,8 +12,8 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
 import javafx.util.Duration;
+import uk.ac.cam.mcksj.Middle;
 import uk.ac.cam.mcksj.WeatherState;
-import uk.ac.cam.mcksj.WeekDay;
 
 import java.util.Calendar;
 import java.util.LinkedList;
@@ -21,24 +21,28 @@ import java.util.LinkedList;
 public class HomePage {
 
     private LinkedList<WeatherNode> weatherNodes = new LinkedList<>();
-
-    //TODO: don't define this here u donut
-    private WeatherState selectedWeather = new WeatherState(2,18,5, 0.12f,WeekDay.SATURDAY, 15);
-
+    
     private Calendar calendar = Calendar.getInstance();
 
     private WeekdayButton[] weekdayPanes = new WeekdayButton[7];
-    private int currentPane = 0;
+    private int currentDayPane = 0;
+
+    //up to date hour for greying out invalid times
+    private int currentHour;
 
     private TimeButton[] timePanes = new TimeButton[24];
     private int currentTimePane;
 
     private Scene mainScene;
     private Scene settingsScene;
+    
+    private Middle weatherInteface;
+    private WeatherState focusState;
 
+    public HomePage(Stage primaryStage, int currentHour, Calendar calendar, Middle weatherInterface) {
+        this.weatherInteface = weatherInterface;
 
-
-    public HomePage(Stage primaryStage, int currentHour, Calendar calendar) {
+        focusState = weatherInterface.getWeather(0,currentHour);
         //grid for days of the week
         GridPane dayGridPane = new GridPane();
         dayGridPane.setLayoutY(800-68);
@@ -50,11 +54,35 @@ public class HomePage {
             button.getPane().setOnMouseClicked(new EventHandler<MouseEvent>() {
                 @Override
                 public void handle(MouseEvent event) {
-                    //TODO update weather with API call using current time and button.getWeekDay() and update nodes
+                    //update weather with API call using current time and button.getWeekDay() and update nodes
+                    focusState = weatherInterface.getWeather(button.getBarIndex(),currentTimePane);
+                    updateNodes();
+                    
 
-                    weekdayPanes[currentPane].setColor(ColourScheme.DARK_BROWN);
+
+                    //change colour of weekday buttons
+                    if (currentDayPane == 0 && button.getBarIndex() != 0) {
+                        //colour buttons brown
+                        for (int i = 0; i < 24; i++) {
+                            timePanes[i].setColor(ColourScheme.DARK_BROWN);
+                        }
+                        timePanes[currentTimePane].setColor(ColourScheme.LIGHT_BROWN);
+                    } else if (currentDayPane != 0 && button.getBarIndex() == 0) {
+                        //colour grey
+                        for (int i = 0; i < currentHour; i++) {
+                            timePanes[i].setColor(ColourScheme.DARK_GREY);
+                        }
+
+                        //select least valid time
+                        if (currentTimePane < currentHour) currentTimePane = currentHour;
+                        focusState = weatherInterface.getWeather(currentDayPane, currentHour);
+                        timePanes[currentHour].setColor(ColourScheme.LIGHT_BROWN);
+                        updateNodes();
+                    }
+
+                    weekdayPanes[currentDayPane].setColor(ColourScheme.DARK_BROWN);
                     button.setColor(ColourScheme.LIGHT_BROWN);
-                    currentPane = button.getBarIndex();
+                    currentDayPane = button.getBarIndex();
                 }
             });
             dayGridPane.add(button.getPane(),i,0);
@@ -80,20 +108,23 @@ public class HomePage {
             time.getPane().setOnMouseClicked(new EventHandler<MouseEvent>() {
                 @Override
                 public void handle(MouseEvent event) {
-                    //TODO: update time by querying API with selectedTime and current Weekday
-                    int selectedTime = time.getTime();
-                    selectedWeather = new WeatherState(5,100,5,0.12f, WeekDay.SATURDAY,10);
-                    updateNodes();
 
-                    final Timeline timeline = new Timeline();
-                    final KeyValue kv = new KeyValue(timeBarPane.hvalueProperty(), ((time.getTime()-3.0))*(1.0/17.0));
-                    final KeyFrame kf = new KeyFrame(Duration.millis(500), kv);
-                    timeline.getKeyFrames().add(kf);
+                    if (!(currentDayPane == 0 && !time.isValidTime())) {
+                        //update time by querying API with selectedTime and current Weekday
+                        int selectedTime = time.getTime();
+                        focusState = weatherInterface.getWeather(currentDayPane, selectedTime);
+                        updateNodes();
 
-                    time.setColor(ColourScheme.LIGHT_BROWN);
-                    timePanes[currentTimePane].setColor(ColourScheme.DARK_BROWN);
-                    currentTimePane = selectedTime;
-                    timeline.play();
+                        final Timeline timeline = new Timeline();
+                        final KeyValue kv = new KeyValue(timeBarPane.hvalueProperty(), ((time.getTime() - 3.0)) * (1.0 / 17.0));
+                        final KeyFrame kf = new KeyFrame(Duration.millis(500), kv);
+                        timeline.getKeyFrames().add(kf);
+
+                        time.setColor(ColourScheme.LIGHT_BROWN);
+                        timePanes[currentTimePane].setColor(ColourScheme.DARK_BROWN);
+                        currentTimePane = selectedTime;
+                        timeline.play();
+                    }
                 }
             });
 
@@ -117,23 +148,23 @@ public class HomePage {
         quadGrid.setLayoutY(124);
 
         //Insert weather nodes
-        WeatherNode rating = new RatingNode(selectedWeather);
+        WeatherNode rating = new RatingNode(focusState);
         quadGrid.add(rating, 0,0,2,1);
         weatherNodes.add(rating);
 
-        WeatherNode temperature = new TemperatureNode(selectedWeather);
+        WeatherNode temperature = new TemperatureNode(focusState);
         quadGrid.add(temperature, 0,1);
         weatherNodes.add(temperature);
 
-        WeatherNode wind = new WindNode(selectedWeather);
+        WeatherNode wind = new WindNode(focusState);
         quadGrid.add(wind, 0,2);
         weatherNodes.add(wind);
 
-        WeatherNode rain = new RainNode(selectedWeather);
+        WeatherNode rain = new RainNode(focusState);
         quadGrid.add(rain, 1,1);
         weatherNodes.add(rain);
 
-        WeatherNode visibility = new VisibilityNode(selectedWeather);
+        WeatherNode visibility = new VisibilityNode(focusState);
         quadGrid.add(visibility, 1,2);
         weatherNodes.add(visibility);
 
@@ -144,7 +175,7 @@ public class HomePage {
         settingsPane.setLayoutY(0);
         settingsPane.setMinSize(68,68);
         settingsPane.setMaxSize(68,68);
-        settingsPane.setStyle("-fx-background-color: #111");
+        settingsPane.setStyle("-fx-background-image: url('uk/ac/cam/mcksj/img/settings_option3_on.png');");
         settingsPane.setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent event) {
@@ -183,12 +214,24 @@ public class HomePage {
         mainScene = new Scene(root, 480, 800);
         mainScene.getStylesheets().add("uk/ac/cam/mcksj/front/styles.css");
 
+        updatePage();
     }
 
     public void updateNodes() {
         for (WeatherNode node : weatherNodes) {
-            node.update(selectedWeather);
+            node.update(focusState);
         }
+    }
+
+    public void updatePage() {
+        currentHour = calendar.get(Calendar.HOUR_OF_DAY);
+        for (int i = 0; i < currentHour; i++) {
+            timePanes[i].setValid(false);
+        }
+        for (int i = currentHour; i < 24; i++) {
+            timePanes[i].setValid(true);
+        }
+
     }
 
     public Scene getMainScene() {
