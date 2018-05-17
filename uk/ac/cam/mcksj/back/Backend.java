fb4b0cdc90e0aab990e60643241a6f4deeaf6510
SaarasM <39161926+SaarasM@users.noreply.github.com>
2018-05-17 14:28:35 +0100

    OpenWeatherMapAPI update

--- uk/ac/cam/mcksj/back/Backend.java
+++ uk/ac/cam/mcksj/back/Backend.java
@@ -17,6 +17,9 @@ public class Backend implements Middle {
 
     private RaspAPI rasp;
 
+    private OpenWeatherMapAPI owm;
+
+
     public Backend() {
         rasp = new RaspAPI();
         weatherCache = new WeatherState[7][MAX_SUPPORTED_TIME + 1];
--- uk/ac/cam/mcksj/back/OpenWeatherMapAPI.java
+++ uk/ac/cam/mcksj/back/OpenWeatherMapAPI.java
@@ -7,42 +7,76 @@ import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
 
 public class OpenWeatherMapAPI {
-    void update(){
+    String update(){
+        String hourly_data = "";
         try {
-            URL url = new URL("http://api.openweathermap.org/data/2.5/weather?lat=51.0&lon=1.0&appid=289d10d3384fffe3eaec60ea7d27f8c6");
+            URL url = new URL("http://api.openweathermap.org/data/2.5/forecast?lat=51.0&lon=1.0&appid=289d10d3384fffe3eaec60ea7d27f8c6");
             InputStream in = url.openConnection().getInputStream();
 
             ReadableByteChannel readChannel = Channels.newChannel(in);
-            ByteBuffer buffer = ByteBuffer.allocate(2048);
+            ByteBuffer buffer = ByteBuffer.allocate(20000);
+            readChannel.read(buffer);
             readChannel.read(buffer);
 
-            int bracketDepth = 0;
-            for(int i=0; i<buffer.array().length; i++) {
+            for (int i = 0; i < buffer.array().length; i++) {
                 int iAsciiValue = buffer.array()[i];
-                String strAsciiTab = Character.toString((char) iAsciiValue);
-                if(strAsciiTab.equals("{")){
-                    bracketDepth += 1;
-                }
-                else if (strAsciiTab.equals("}")){
-                    bracketDepth -= 1;
-                }
-
-                System.out.print(strAsciiTab);
-                if (bracketDepth == 0) {
-                    break;
-                }
-                if (strAsciiTab.equals(",") && bracketDepth<=1){
-                    System.out.println();
-                }
+                hourly_data += Character.toString((char) iAsciiValue);
             }
         }
+
         catch(Exception e){
             System.out.println(e);
         }
+
+        return hourly_data;
     }
 
+    void parse(String data){
+        char[] charArray = data.toCharArray();
+
+        int bracketDepth = 0;
+        int index = 0;
+        String currString = "";
+        String[] stringArray = new String[50];
+
+        int stringArrayIndex = 0;
+        for(int i=0; i<charArray.length; i++) {
+            index+=1;
+            if(charArray[i] == '{' || charArray[i] == '['){
+                bracketDepth += 1;
+            }
+            else if (charArray[i] == '}' || charArray[i] == ']'){
+                bracketDepth -= 1;
+            }
+
+            currString += charArray[i];
+
+            if (bracketDepth == 2 && (charArray[i] == '}' || charArray[i] == ']')) {
+                stringArray[stringArrayIndex] = currString;
+                stringArrayIndex += 1;
+                currString = "";
+            }
+
+            if (bracketDepth==0){
+                break;
+            }
+        }
+        for(String s: stringArray) {
+            System.out.println(s);
+        }
+    }
+
+
+
     public static void main(String[] args){
-        Backend back = new Backend();
-        back.updateWeather();
+        OpenWeatherMapAPI owmapi = new OpenWeatherMapAPI();
+        String data = owmapi.update();
+        owmapi.parse(data);
+
+
+
+        // To Do:
+        //  Get hourly weather for next week
+        //  Parse response
     }
 }
